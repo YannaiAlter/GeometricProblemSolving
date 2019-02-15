897aec3177eba070dc76f715eaaec8547b06bc33
yannaialter <yanayalter@gmail.com>
2019-02-15 18:58:15 +0200

    Added Documantation

--- src/Angle.java
+++ src/Angle.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Angle {
+public class Angle { //This is angle class
 	Dot firstDot,secondDot,thirdDot;
 	Angle(Dot first,Dot middle,Dot end)
 	{
